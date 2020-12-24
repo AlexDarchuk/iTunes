14cf9eae95fc1ac73c8e91b5a077b7d6774e39be
Alex <o.darchuk@gmail.com>
2020-12-24 13:16:01 +0200

    second commit

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -1,3 +1,12 @@
 export const musicPlayerInit = () => {
-    console.log('music');
+    const audio = document.querySelector('.audio');
+    const audioImg = document.querySelector('.audio-img');
+    const audioHeader = document.querySelector('.audio-header');
+    const audioPlayer = document.querySelector('.audio-player');
+    const audioNavigation = document.querySelector('.audio-navigation');
+    const audioButtonPlay = document.querySelector('.audio-button__play');
+    const audioProgress = document.querySelector('.audio-progress');
+    const audioProgressTiming = document.querySelector('.audio-progress__timing');
+    const audioTimePassed = document.querySelector('.audio-time__passed');
+    const audioTimeTotal = document.querySelector('.audio-time__total');
 };
